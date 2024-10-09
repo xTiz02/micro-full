a9cfc57649edc5e1a77b55e185dc011f198b0cf2
Piero Alessandro Rodriguez Diaz <u21231696@utp.edu.pe>
2024-10-08 19:55:40 -0500

    Comentarios de configuración

--- eureka-server/src/main/java/org/prd/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/prd/eurekaserver/EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
-
+    //Servidor de registro de servicios
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
